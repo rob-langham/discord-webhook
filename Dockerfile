19a4dc463f0f1822f3c6d0f1e93a4788e96bd92e
Robert Langham <rlangham-github.f97dy@slmails.com>
2023-10-04 14:49:04 +0100

    Simple working service with docker compose

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18
+# Create app directory
+WORKDIR /usr/src/app
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
+RUN npm ci
+# If you are building your code for production
+# RUN npm ci --omit=dev
+# Bundle app source
+COPY . .
+EXPOSE 3000
+CMD [ "npm", "run", "serve" ]
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,36 @@
 // src/app.ts
 import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
+app.use(bodyParser.text({ type: "*/*" }));
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const id = process.env.WEBHOOK_ID;
+const token = process.env.WEBHOOK_TOKEN;
+
+app.post("/", async (req, res) => {
+  const webhook = `https://discord.com/api/webhooks/${id}/${token}`;
+
+  try {
+    await axios.post(webhook, {
+      content: req.body,
+    });
+
+    res.status(200).send("");
+  } catch (error) {
+    const time = Date.now();
+    console.error(time, error);
+    res.status(500).send(`Error @ ${time}`);
+  }
 });
+
+if (!id || !token) {
+  console.error("Missing Discord Webhook ID or Token");
+  process.exit(1);
+}
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
