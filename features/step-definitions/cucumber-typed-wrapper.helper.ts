53460281a29a10a3a96ee40cf910382d54b7c6e7
Robert Langham <rlangham-github.f97dy@slmails.com>
2023-12-09 01:14:58 +0000

    Start implementing scenarios

--- features/step-definitions/cucumber-typed-wrapper.helper.ts
+++ features/step-definitions/cucumber-typed-wrapper.helper.ts
@@ -21,18 +21,16 @@ type obj = { [key: string]: any };
 type World<Params, Context> = Pick<IWorld<Params>, "attach" | "log" | "parameters"> & Context;
 //typescript wrapper for cucumber's Given function
 
-function typedStepWrapper<Context = obj, Params = obj>(step: IDefineStep) {
-  return (
-    pattern: string,
-    fn: (this: World<Params, Context>, ...args: Params[]) => void | "pending" | Promise<any>
-  ) => step(pattern, fn);
+function typedStepWrapper<Context = obj>(step: IDefineStep) {
+  return (pattern: string, fn: (this: World<any, Context>, ...args: any[]) => any | Promise<any>) =>
+    step(pattern, fn);
 }
 
-export function typedSteps<Context = obj, Params = obj>() {
+export function typedSteps<Context = obj>() {
   return {
-    given: typedStepWrapper<Context, Params>(Given),
-    when: typedStepWrapper<Context, Params>(When),
-    then: typedStepWrapper<Context, Params>(Then),
+    given: typedStepWrapper<Context>(Given),
+    when: typedStepWrapper<Context>(When),
+    then: typedStepWrapper<Context>(Then),
     before: Before, // TODO
     beforeAll: BeforeAll, // TODO
     after: After, // TODO
--- features/step-definitions/hooks.ts
+++ features/step-definitions/hooks.ts
@@ -1,10 +1,11 @@
+import { BeforeAll } from "@cucumber/cucumber";
 import { mock } from "pactum";
 
 // sets port, host
 const port = +(process.env.PORT || "3001");
 const mockOpts = { port, host: "127.0.0.1" };
 
-async function startMock() {
+BeforeAll(async () => {
   await mock.setDefaults(mockOpts);
   await mock.start();
-}
+});
